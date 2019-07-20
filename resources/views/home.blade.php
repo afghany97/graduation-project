a234a2e56ca65087b67e7ce55cd582105f8cef9d
afghany <muhammadreda97@gmail.com>
2019-07-20 21:06:11 +0200

    change list of student subjects layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="subjects">
-        <div class="container">
-            <h2 class="text-center">Subjects</h2>
 
+    <section class="subjects">
+
+        <div class="container">
+
+            <h2 class="text-center">Subjects</h2>
 
             <div class="row">
 
                 @foreach($subjects as $subject)
-                    <div class="col-lg-3 col-md-4 col-sm-12">
+
+                    <div class="col-lg-8 col-md-8 col-sm-8 mx-auto">
+
                         <div class="card {{$subject->isQuestionnaired() ? 'dis': ''}}">
 
                             <a href="{{route('questionnaires.create',$subject)}}" class="poll-icon">
+
                                 <i class="fas fa-poll"></i>
+
                             </a>
 
                             <div class="mask"></div>
 
                             <div class="card-body">
-                                <h5 class="card-title text-center">{{$subject->name}}</h5>
 
+                                <h5 class="card-title text-center">{{$subject->name}}</h5>
 
                             </div>
 
                     </div>
+
                 </div>
+
                 @endforeach
 
-
             </div>
+
         </div>
+
     </section>
+
 @endsection
